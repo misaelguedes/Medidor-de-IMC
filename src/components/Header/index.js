2b5bb6b15f79eab5e9a0f1345d301f7b29e3fc80
Misael Guedes de Lima <124177593+misaelguedes@users.noreply.github.com>
2023-10-27 20:35:51 -0300

    Criando o Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import './header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Medidor de IMC (Índice de Massa Corporal)</h1>
+        </header>
+    )
+}
